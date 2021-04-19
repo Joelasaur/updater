08017ec38e1c396d2e3eeb1cc9671d0b3b1a4d36
Joelasaur <jowhalen13@gmail.com>
2021-04-19 15:35:30 -0400

    rename commands file to js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import * as commands from './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
